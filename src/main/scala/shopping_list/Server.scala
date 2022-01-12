9e25baff8fa385ffcfa3c1d50b11aca1673b55b7
unknown <wtm.markowski@gmail.com>
2022-01-12 16:04:00 +0000

    Started the Server.scala (using the configs to create a DB transactor)

---
+++ src/main/scala/shopping_list/Server.scala
@@ -0,0 +1,31 @@
+package shopping_list
+
+import cats.effect._
+import cats.syntax.functor._
+import doobie.util.ExecutionContexts
+import io.circe.config.parser
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.{Server => H4Server}
+
+import shopping_list.config.{DatabaseConfig,ShoppingListConfig}
+
+
+
+object Server extends IOApp {
+
+  def createServer[F[_] :ContextShift: ConcurrentEffect: Timer] : Resource[F, H4Server[F]] =
+
+  for {
+    conf <- Resource.liftF(parser.decodePathF[F, ShoppingListConfig]("petstore"))
+    fixedThreadPool  <- ExecutionContexts.fixedThreadPool[F](conf.database.connections.poolSize)
+    cachedThreadPool <- ExecutionContexts.cachedThreadPool[F]
+    _ <- DatabaseConfig.transactor(conf.database, fixedThreadPool, cachedThreadPool)
+    server <- BlazeServerBuilder[F]
+        .bindHttp(conf.server.port, conf.server.host)
+        .resource
+    } yield server
+  
+
+  def run(args:List[String]): IO[ExitCode] = 
+    createServer.use(_ => IO.never).as(ExitCode.Success)
+}
