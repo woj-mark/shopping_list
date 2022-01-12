8b3bf0100bf7a48a75abb3fac8151d1cca550825
unknown <wtm.markowski@gmail.com>
2022-01-12 23:34:40 +0000

    Added Errors defining the business logic

--- src/main/scala/shopping_list/Server.scala
+++ src/main/scala/shopping_list/Server.scala
@@ -20,6 +20,8 @@ object Server extends IOApp {
     fixedThreadPool  <- ExecutionContexts.fixedThreadPool[F](conf.database.connections.poolSize)
     cachedThreadPool <- ExecutionContexts.cachedThreadPool[F]
     _ <- DatabaseConfig.transactor(conf.database, fixedThreadPool, cachedThreadPool)
+    transactor <- DatabaseConfig.transactor(conf.database, fixedThreadPool, cachedThreadPool)
+_          = DoobieShoppingListInterpreter[F](transactor)
     server <- BlazeServerBuilder[F]
         .bindHttp(conf.server.port, conf.server.host)
         .resource
---
+++ src/main/scala/shopping_list/data/ValidationError.scala
@@ -0,0 +1,17 @@
+package shopping_list
+
+import shopping_list.data.Item
+
+
+/**
+ * Definition of Validation Errors to be used in the shopping list application
+ * This allow separation of logical errors from business errors (defined with own ADT)
+ */
+
+sealed trait ValidationError extends Item with Serializable
+
+case class ItemAlreadyExistsError(item:Item) extends ValidationError
+
+case object ItemNotFoundError extends ValidationError
+
+
